b3581de784fa559f435431579b88537571d5480f
mcbalaam <mr.max.kh@gmail.com>
2025-12-02 19:45:41 +0700

    prettyyy

--- src/App.tsx
+++ src/App.tsx
@@ -13,51 +13,54 @@ import Badge from "./components/Badge";
 import StatusBubble from "./components/StatusBubble";
 import Timestamp from "./components/Timestamp";
 import Tooltip from "./components/Tooltip";
+import RepoTab from "./components/RepoTab";
 
 export function App() {
   return (
-    <div className="card">
-      <div className="banner">
-        <img className="pfp" src={pfp}></img>
-        <StatusBubble>
-          busy auracoding
-        </StatusBubble>
-      </div>
-      <div className="item-container">
-        <div className="name">
-          <div className="nameplate">mcbalaam</div>
-          <Badge small src={extera}>
-            extera
-          </Badge>
+    <div className="master-container">
+      <div className="card">
+        <div className="banner">
+          <img className="pfp" src={pfp}></img>
+          <StatusBubble>
+            busy auracoding
+          </StatusBubble>
         </div>
-        <div className="pnouns">
-          mcbalaam ~ эмсибалаам, балаам, макбаклак (he/him)
-        </div>
-        <p className="desc">hey there. name's Max, I'm learning web development and Rust in shape of microservices and GUI apps. I contribute small features to /tg/station from time to time, write Telegram bots and help set them up. open for bounties -w-</p>
-        <div style={{display: 'flex', height: 'fit-content', margin: '0px 5px 10px 0'}}>
-          <p>My midnight is your</p> <Timestamp ts="1764608400" />
-        </div>
+        <div className="item-container">
+          <div className="name">
+            <div className="nameplate">mcbalaam</div>
+            <Badge small src={extera}>
+              extera
+            </Badge>
+          </div>
+          <div className="pnouns">
+            mcbalaam ~ эмсибалаам, балаам, макбаклак (he/him)
+          </div>
+          <p className="desc">hey there. name's Max, I'm learning web development and Rust in shape of microservices and GUI apps. I contribute small features to /tg/station from time to time, write Telegram bots and help set them up. open for bounties -w-</p>
+          <div style={{display: 'flex', height: 'fit-content', margin: '0px 5px 10px 0'}}>
+            <p>My midnight is your</p> <Timestamp ts="1764608400" />
+          </div>
 
-        <h1>Connections</h1>
-        <div className="connections">
-          <Badge href="https://www.github.com/mcbalaam" src={github}>
-            GitHub ↗
-          </Badge>
-          <Badge href="https://steamcommunity.com/id/mcbalaam/" src={steam}>
-            Steam ↗
-          </Badge>
-          <Badge href="https://soundcloud.com/mcbalaam" src={soundcloud}>
-            Soundcloud ↗
-          </Badge>
-          <Badge href="https://t.me/whattheactualfuckbro" src={soundcloud}>
-            Telegram ↗
-          </Badge>
+          <h1>Connections</h1>
+          <div className="connections">
+            <Badge href="https://www.github.com/mcbalaam" src={github}>
+              GitHub ↗
+            </Badge>
+            <Badge href="https://steamcommunity.com/id/mcbalaam/" src={steam}>
+              Steam ↗
+            </Badge>
+            <Badge href="https://soundcloud.com/mcbalaam" src={soundcloud}>
+              Soundcloud ↗
+            </Badge>
+            <Badge href="https://t.me/whattheactualfuckbro" src={soundcloud}>
+              Telegram ↗
+            </Badge>
+          </div>
+          {/*<div className="buttons">
+            <Button fill>Authorize with GitHub</Button>
+            <Button contrast>Sign</Button>
+            <Button>About signs</Button>
+          </div>*/}
         </div>
-        {/*<div className="buttons">
-          <Button fill>Authorize with GitHub</Button>
-          <Button contrast>Sign</Button>
-          <Button>About signs</Button>
-        </div>*/}
       </div>
     </div>
   );
